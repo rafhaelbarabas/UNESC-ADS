dd3db0a10e48e9e862434715adc8942a882abbf0
Rafhael de Andrade Barabas <rafhael_ab@hotmail.com>
2021-09-19 21:42:34 -0300

    layouts

---
+++ 7-Fase/Web Mobile/CAIXA-UI/app/src/main/java/com/rafhael/caixa/AvaliacaoActivity.java
@@ -0,0 +1,14 @@
+package com.rafhael.caixa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvaliacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao);
+    }
+}
